e52090036c792b454519084b79f69be6e7afb9bc
Cotbert2 <mateojgdev@gmail.com>
2024-11-06 09:13:59 -0500

    fp1 info

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,20 @@ export class Tab1Page {
 
   constructor() {}
 
+  fanProject1ModalVisible : boolean = true;
+  fanProject2ModalVisible : boolean = false;
+  fanProject3ModalVisible : boolean = false;
+
+  changeStatusFP1 () : void {
+    this.fanProject1ModalVisible = !this.fanProject1ModalVisible;
+  }
+
+  changeStatusFP2 () : void {
+    this.fanProject1ModalVisible = !this.fanProject1ModalVisible;
+  }
+
+  changeStatusFP3 () : void {
+    this.fanProject1ModalVisible = !this.fanProject1ModalVisible;
+  }
+
 }
