c29e2f4be9c9a7bf5298a3695580e6a45ff64fd2
Cotbert2 <mateojgdev@gmail.com>
2024-11-07 11:42:37 -0500

    about page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,13 +9,23 @@ const { Share } = Plugins;
 })
 export class Tab2Page {
 
-  async basicShare() {
+
+  linkWhatsApp : string = "https://chat.whatsapp.com/JdtOJ9i67Lk1S13j8qMhXm";
+  linkInstagram : string = "https://www.instagram.com/linkinparkcolombia/";
+  linkPlayList : string = "https://open.spotify.com/playlist/3DMnCkMO8GJKPf6wMkBpYm?si=xRQ8_8MMR3CSEZTdddrGzw";
+  linkGithub : string = "https://github.com/Cotbert2/";
+
+  async openUrlbasicShare() {
     await Share.share({
-      title: 'Share my favorite app',
-      text: 'This app is awesome, try it out!',
-      url: 'https://ionicframework.com/docs/native/share',
-      dialogTitle: 'Share with buddies'
+      title: 'Fan Projects LP Colombia 2024',
+      text: 'Conoce más sobre los proyectos de la comunidad de fans de Linkin Park en Colombia',
+      url : 'https://drive.google.com/file/d/1eUaCOahlKg0zUyAyzrw1_IDhHXZm9Gko/view',
+      dialogTitle: 'Linkin Park Colombia'
     });
+  }
+
+  openUrl (url : string) : void {
+    window.open(url, '_blank');
   }
 
 
