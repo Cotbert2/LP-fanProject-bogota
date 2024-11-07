a02a982cc62ca92a75cbe5a678d4baa56f9af900
Cotbert2 <mateojgdev@gmail.com>
2024-11-07 16:45:10 -0500

    logos

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'LP-FanProject',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Duración del splash screen en milisegundos
+      launchAutoHide: true,     // Oculta automáticamente el splash screen
+      backgroundColor: '#ffffff', // Color de fondo
+      androidSplashResourceName: 'splash', // Nombre de la imagen en Android
+      iosSplashResourceName: 'Default', // Nombre de la imagen en iOS
+      showSpinner: true,         // Muestra un spinner
+      spinnerColor: '#999999'    // Color del spinner
+    },
+  }
 };
 
 export default config;
